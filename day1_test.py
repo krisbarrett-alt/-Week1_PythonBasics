bf8ec88c7350c22d6668a6d244139221cf7458a1
Kris Barrett <krisbarrett@Kriss-Air.lan>
2025-10-17 12:38:47 -0400

    Initial commit

---
+++ day1_test.py
@@ -0,0 +1,29 @@
+print("Hello, Python!")
+name = "kris"
+age = 39
+print("Name:", name)
+print("Age:", age)
+
+# Numbers
+x = 10
+y = 3
+
+# String
+greeting = "Hello"
+
+# Boolean
+is_student = True
+
+print(x, y, greeting, is_student)
+
+#Input
+user_name = input("Enter your name: ")
+print("Hello,", user_name)
+
+#Basic Operations
+a = 5
+b = 3
+print("Add:", a + b)
+print("Subtract:", a - b)
+print("Multiply", a * b)
+print("Divide", a / b)
